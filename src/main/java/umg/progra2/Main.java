c91cddc326d60074922ab94398ccf6c68c2bdb79
Edgar <5910usuario@gmail.com>
2024-08-02 21:05:51 -0600

    Proyecto terminado incluso con la clase exterior.

--- src/main/java/umg/progra2/Main.java
+++ src/main/java/umg/progra2/Main.java
@@ -1,5 +1,8 @@
 package umg.progra2;
 
+import conductores.Conductor;
+import conductores.ConductorDeMoto;
+import conductores.CondutorDeCoche;
 import vehiculos.Coche;
 import vehiculos.Moto;
 import vehiculos.Vehiculo;
@@ -11,6 +14,7 @@ public class Main {
         Vehiculo coch = new Coche("Toyota","Raw4",1998, 4 );
         Vehiculo mot = new Moto("Honda", "Bla bla", 2010, true);
 
+
         System.out.println("=====================\n");
         coch.mostrardetalles();
         System.out.println("\n=====================");
@@ -18,5 +22,19 @@ public class Main {
         System.out.println("=====================\n");
         mot.mostrardetalles();
         System.out.println("\n=====================");
+
+        System.out.println("\n===================== Area de Trabajo de la clase agregada =====================\n");
+
+        Conductor con_c = new CondutorDeCoche("Alex", "A59104667", 5);
+        Conductor con_m = new ConductorDeMoto("Manuel", "B59104667", true);
+
+        System.out.println("=====================\n");
+        con_c.mostrarinformacion();
+        System.out.println("\n=====================");
+
+        System.out.println("=====================\n");
+        con_m.mostrarinformacion();
+        System.out.println("\n=====================");
+
     }
 }
